c60159e39533aa1ad5dfb3bf862e2c4b8fdef667
“Massonus“ <maksym.lithuanian@gmail.com>
2024-06-05 22:03:23 +0300

    refactor

--- db/postgresql_pandas.py
+++ db/postgresql_pandas.py
@@ -4,6 +4,7 @@ import pandas as pd
 engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:root@localhost:5432/Test")
 # engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:massonus@awseb-e-umvcxak4y3-stack-awsebrdsdatabase-9ldqegjv6dyg.cps2ceiewud2.eu-central-1.rds.amazonaws.com:5432/postgres")
 
+print(pd.read_sql('employee', engine))
 
 df1 = pd.DataFrame([{'name': 'asdfg'}])
 df1.to_sql('employee', engine, if_exists='append', index=False, index_label='id')
