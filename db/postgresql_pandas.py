8c18841f8b7a4ac887f9f35cfc35de76e2de569f
“Massonus“ <maksym.lithuanian@gmail.com>
2024-06-05 22:02:46 +0300

    added practice with pandas

---
+++ db/postgresql_pandas.py
@@ -0,0 +1,9 @@
+import sqlalchemy
+import pandas as pd
+
+engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:root@localhost:5432/Test")
+# engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:massonus@awseb-e-umvcxak4y3-stack-awsebrdsdatabase-9ldqegjv6dyg.cps2ceiewud2.eu-central-1.rds.amazonaws.com:5432/postgres")
+
+
+df1 = pd.DataFrame([{'name': 'asdfg'}])
+df1.to_sql('employee', engine, if_exists='append', index=False, index_label='id')
