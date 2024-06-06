83e37762a9a9d3489d75e95b6f9ef3533cd86eea
“Massonus“ <maksym.lithuanian@gmail.com>
2024-06-06 22:35:52 +0300

    refactor pandas practice

--- db/postgresql_pandas.py
+++ db/postgresql_pandas.py
@@ -4,13 +4,19 @@ import pandas as pd
 engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:root@localhost:5432/Test")
 # engine = sqlalchemy.create_engine("postgresql+psycopg2://postgres:massonus@awseb-e-umvcxak4y3-stack-awsebrdsdatabase-9ldqegjv6dyg.cps2ceiewud2.eu-central-1.rds.amazonaws.com:5432/postgres")
 
-data = pd.read_sql('employee', engine)
+# data = pd.read_sql('employee', engine)
+#
+# index = data[data['id'] == 1].index
+#
+# data.loc[index, 'name'] = 'Jordan'
+# data.to_sql('employee', engine, if_exists='replace', index=False, index_label='id')
+# # df1 = pd.DataFrame([{'name': 'asdfg'}])
+# # df1.to_sql('employee', engine, if_exists='append', index=False, index_label='id')
+# data = data.drop(index)
+# data.to_sql('employee', engine, if_exists='replace', index=False, index_label='id')
 
-index = data[data['id'] == 1].index
-
-data.loc[index, 'name'] = 'Jordan'
-data.to_sql('employee', engine, if_exists='replace', index=False, index_label='id')
-# df1 = pd.DataFrame([{'name': 'asdfg'}])
-# df1.to_sql('employee', engine, if_exists='append', index=False, index_label='id')
-data = data.drop(index)
-data.to_sql('employee', engine, if_exists='replace', index=False, index_label='id')
+# data = pd.read_sql('pending_users', engine)
+# index = data[data['user_id'] == 661204444].index
+# print(index)
+# data = data.drop(index)
+# data.to_sql('pending_users', engine, if_exists='replace', index=False, index_label='id')
